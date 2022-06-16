39f4cac46c831743e32893b110dffd9e25c62006
Bagasarfiansyah007 <bagasarfiansyah007@gmail.com>
2022-06-16 21:18:37 +0700

    Commit file

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -7,13 +7,9 @@ public class Driver {
 		Counter counter = new Counter();
 		
 		System.out.println("Current count:" + counter.getCount());
-		
 		counter.increment();
-		
 		System.out.println("Current count:" + counter.getCount());
-		
 		counter.decrement();
-		
 		System.out.println("Current count:" + counter.getCount());
 		
 	}
